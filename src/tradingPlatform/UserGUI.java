25e6c461dafe992762f37c674636f4f2ac3d3f46
Sherry-1015 <n10351680@qut.edu.au>
2021-05-14 14:51:09 +1000

    update the client side gui

--- src/tradingPlatform/UserGUI.java
+++ src/tradingPlatform/UserGUI.java
@@ -69,7 +69,7 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
     private void ResourcePane(JTabbedPane tabbedPane) {
         Container resource = new Container();
         tabbedPane.addTab("Resource", resource);
-        resource.setBackground(Color.decode("#0b2862"));
+        resource.setBackground(Color.decode("#c8ddf2"));
         resource.setLayout(new BoxLayout(resource, BoxLayout.Y_AXIS));
 
         resource.add(searchFieldPanel());
@@ -79,33 +79,44 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
 
     private JPanel searchFieldPanel() {
         JPanel searchField = new JPanel();
+        searchField.setBackground(Color.decode("#0b2862"));
         JTextField tf = new JTextField(30);
         JButton search = new JButton("Search");
         searchField.add(tf);
         searchField.add(search);
+        searchField.setMaximumSize(new Dimension(1000, 50));
         return searchField;
     }
 
     private JPanel buttonFieldPanel() {
         JPanel buttonField = new JPanel();
+        buttonField.setBackground(Color.decode("#c8ddf2"));
         JButton button_1 = new JButton("Computational Resource");
         JButton button_2 = new JButton("Hardware Resource");
         JButton button_3 = new JButton("Software Resource");
         buttonField.add(button_1);
         buttonField.add(button_2);
         buttonField.add(button_3);
+        buttonField.setMaximumSize(new Dimension(500, 80));
         return buttonField;
     }
 
     private JScrollPane resourceScrollPane() {
         String data[][] = {
-                {"101", "Amit123", "Amit", "Jack", "Sales", "$670000", "BUY"},
-                {"102", "Jai007", "Jai", "Spring", "Human Resource", "$780000", "BUY"},
-                {"101", "Sachin334", "Sachin", "Eystein", "Design", "$700000", "BUY"}};
+                {"101", "Amit123", "Amit", "Jack", "Sales", "$670000"},
+                {"102", "Jai007", "Jai", "Spring", "Human Resource", "$780000"},
+                {"101", "Sachin334", "Sachin", "Eystein", "Design", "$700000"}};
 
-        String column[] = {"ID", "USERNAME", "FIRST NAME", "LAST NAME", "ORGANIZATION", "PRICE", "Button"};
+        String column[] = {"ID", "USERNAME", "FIRST NAME", "LAST NAME", "ORGANIZATION", "CURRENT PRICE"};
         resourceTable = new JTable(data, column);
         JScrollPane scrollPane = new JScrollPane(resourceTable);
+        scrollPane.setViewportView(resourceTable);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setMinimumSize(new Dimension(400, 150));
+        scrollPane.setPreferredSize(new Dimension(450, 150));
+        scrollPane.setMaximumSize(new Dimension(700, 200));
+
 
         return scrollPane;
 
@@ -120,10 +131,12 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
 
         offer.add(searchFieldPanel_2());
         offer.add(offersScrollPane());
+        offer.add(offerButtonFieldPanel());
 
     }
     private JPanel searchFieldPanel_2() {
         JPanel searchField_2 = new JPanel();
+        searchField_2.setBackground(Color.decode("#0b2862"));
         JTextField tf = new JTextField(30);
         JButton search = new JButton("Search");
         searchField_2.add(tf);
@@ -133,24 +146,42 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
         JComboBox sourceList = new JComboBox(sourceStrings);
         sourceList.setSelectedIndex(1);
         searchField_2.add(sourceList);
-
+        searchField_2.setMaximumSize(new Dimension(1000, 50));
         return searchField_2;
     }
 
     private JScrollPane offersScrollPane() {
-        String column_1[] = {"Resources", "Organization", "Total", "Available", "Price", "Date", "Button","Button"};
+        String column_1[] = {"Resources ID","Resources","Order time","Organisation","Price","Total","Remaining","Last Updated"};
         String data_1[][] = {
-                {"R1", "Happy Planet", "10,000", "8,000", "$3,000", "27/04/2021", "Edit", "Remove"}};
+                {"R1", "RAM", "27/04/2021","Happy Planet","$3,000","10,000", "8,000",  "27/04/2021"}};
         offerTable = new JTable(data_1, column_1);
         JScrollPane scrollPane_2 = new JScrollPane(offerTable);
+        scrollPane_2.setViewportView(offerTable);
+        scrollPane_2.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane_2.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane_2.setMinimumSize(new Dimension(400, 150));
+        scrollPane_2.setPreferredSize(new Dimension(450, 150));
+        scrollPane_2.setMaximumSize(new Dimension(700, 300));
 
         return scrollPane_2;
     }
+    private JPanel offerButtonFieldPanel() {
+        JPanel buttonField_2 = new JPanel();
+        JButton buyButton = new JButton("Edit");
+        JButton sellButton = new JButton("Remove");
+
+        buttonField_2.add(buyButton);
+        buttonField_2.add(sellButton);
+        buttonField_2.setMaximumSize(new Dimension(500, 50));
+        buttonField_2.setBackground(Color.decode("#c8ddf2"));
+        return buttonField_2;
+    }
+
 
     //start of the sell page
     private void SellPane(JTabbedPane tabbedPane) {
         Container sell = new Container();
-        tabbedPane.addTab("SELL", sell);
+        tabbedPane.addTab("BUY&SELL", sell);
         sell.setBackground(Color.decode("#0b2862"));
         sell.setLayout(new BoxLayout(sell, BoxLayout.Y_AXIS));
         sell.add(titlePanel());
@@ -160,17 +191,21 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
 
     private JPanel titlePanel(){
         JPanel title = new JPanel();
+        title.setBackground(Color.decode("#0b2862"));
         JLabel L1 = new JLabel("Resource Title");
+        L1.setFont(new Font("Verdana", Font.BOLD, 24));
+        L1.setForeground(Color.WHITE);
         title.add(L1);
-        title.setMaximumSize(new Dimension(700, 200));
+        title.setMaximumSize(new Dimension(500, 50));
         return  title;
     }
 
     private JPanel contentPanel(){
         JPanel content = new JPanel();
-
         GroupLayout layout = new GroupLayout(content);
         content.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
 
 
 
@@ -185,7 +220,6 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
         salesAmount = new JTextField(20);
         credits = new JTextField(20);
         estimateCredits = new JTextField(20);
-
         JComboBox resourceList = new JComboBox(sourceList);
         GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
         hGroup.addGroup(layout.createParallelGroup()
@@ -219,7 +253,8 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
                 .addComponent(estimateCreditsFieldLabel)
                 .addComponent(estimateCredits));
         layout.setVerticalGroup(vGroup);
-        content.setMaximumSize(new Dimension(700, 150));
+        content.setMaximumSize(new Dimension(500, 150));
+        content.setBackground(Color.WHITE);
 
         return content;
 
@@ -227,10 +262,13 @@ public class UserGUI extends JFrame implements ActionListener, Runnable {
 
     private JPanel buttonFieldPanel_2() {
         JPanel buttonField_2 = new JPanel();
-        JButton button_4 = new JButton("Create SELL Offer");
+        JButton buyButton = new JButton("BUY");
+        JButton sellButton = new JButton("SELL");
 
-        buttonField_2.add(button_4);
-
+        buttonField_2.add(buyButton);
+        buttonField_2.add(sellButton);
+        buttonField_2.setMaximumSize(new Dimension(500, 50));
+        buttonField_2.setBackground(Color.WHITE);
         return buttonField_2;
     }
 
